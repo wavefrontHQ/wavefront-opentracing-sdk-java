462eebb65577404081548e6c2f2a42b640eb15c1
Vikram Raman <vikram@wavefront.com>
2019-04-16 15:56:43 -0700

    async span reporting

--- src/main/java/com/wavefront/opentracing/reporting/WavefrontSpanReporter.java
+++ src/main/java/com/wavefront/opentracing/reporting/WavefrontSpanReporter.java
@@ -10,6 +10,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -21,15 +22,20 @@ import static com.wavefront.opentracing.common.Constants.DEFAULT_SOURCE;
  *
  * @author Vikram Raman (vikram@wavefront.com)
  */
-public class WavefrontSpanReporter implements Reporter {
+public class WavefrontSpanReporter implements Reporter, Runnable {
   private static final Logger logger =
       Logger.getLogger(WavefrontSpanReporter.class.getName());
 
   private final WavefrontSender wavefrontSender;
   private final String source;
+  private final LinkedBlockingQueue<WavefrontSpan> spanBuffer;
+  private final Thread sendingThread;
+
+  private volatile boolean stop = false;
 
   public static final class Builder {
     private String source;
+    private int maxQueueSize = 50000;
 
     public Builder() {
       this.source = getDefaultSource();
@@ -55,23 +61,61 @@ public class WavefrontSpanReporter implements Reporter {
     }
 
     /**
+     * Set max queue size of in-memory buffer. Incoming spans are dropped if buffer is full.
+     *
+     * @param maxQueueSize Max queue size of in-memory buffer
+     * @return {@code this}
+     */
+    public Builder withMaxQueueSize(int maxQueueSize) {
+      this.maxQueueSize = maxQueueSize;
+      return this;
+    }
+
+    /**
      * Builds a {@link WavefrontSpanReporter} for sending opentracing spans to a
      * WavefrontSender that can send those spans either be a via proxy or direct ingestion.
      *
      * @return {@link WavefrontSpanReporter}
      */
     public WavefrontSpanReporter build(WavefrontSender wavefrontSender) {
-      return new WavefrontSpanReporter(wavefrontSender, this.source);
+      return new WavefrontSpanReporter(wavefrontSender, this.source, this.maxQueueSize);
     }
   }
 
-  private WavefrontSpanReporter(WavefrontSender wavefrontSender, String source) {
+  private WavefrontSpanReporter(WavefrontSender wavefrontSender, String source, int maxQueueSize) {
     this.wavefrontSender = wavefrontSender;
     this.source = source;
+    this.spanBuffer = new LinkedBlockingQueue<>(maxQueueSize);
+
+    sendingThread = new Thread(this, "wavefrontSpanReporter");
+    sendingThread.setDaemon(true);
+    sendingThread.start();
+  }
+
+  @Override
+  public void run() {
+    while (!stop) {
+      try {
+        WavefrontSpan span = spanBuffer.take();
+        send(span);
+      } catch (InterruptedException ex) {
+        if (logger.isLoggable(Level.INFO)) {
+          logger.info("reporting thread interrupted");
+        }
+      } catch (Exception ex) {
+        logger.log(Level.WARNING, "Error processing buffer", ex);
+      }
+    }
   }
 
   @Override
   public void report(WavefrontSpan span) {
+    if (!spanBuffer.offer(span)) {
+      logger.warning("Dropping span: " + span);
+    }
+  }
+
+  private void send(WavefrontSpan span) {
     try {
       WavefrontSpanContext ctx = span.context();
       List<Reference> parentRefs = span.getParents();
@@ -112,6 +156,13 @@ public class WavefrontSpanReporter implements Reporter {
 
   @Override
   public void close() throws IOException {
+    stop = true;
+    try {
+      // wait for 5 secs max
+      sendingThread.join(5000);
+    } catch (InterruptedException ex) {
+      // no-op
+    }
     // flush buffer & close client
     wavefrontSender.close();
   }
