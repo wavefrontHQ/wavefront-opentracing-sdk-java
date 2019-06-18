edb155ac64729a1f7c85fb032b8524344441d046
Sushant Dewan <sushant@wavefront.com>
2019-06-18 01:41:02 -0700

    add support for custom red metric tags (#63)
    * add support for custom red metric tags
    
    * minor name change

--- src/test/java/com/wavefront/opentracing/WavefrontSpanTest.java
+++ src/test/java/com/wavefront/opentracing/WavefrontSpanTest.java
@@ -35,7 +35,7 @@ public class WavefrontSpanTest {
   @Test
   public void testValidWavefrontSpan() throws IOException, InterruptedException {
     String operationName = "dummyOp";
-    Map<String, String> pointTags = pointTags(operationName);
+    Map<String, String> pointTags = pointTags(operationName, new HashMap<>());
     WavefrontSender wfSender = createMock(WavefrontSender.class);
     wfSender.sendSpan(eq(operationName), anyLong(), anyLong(), eq(DEFAULT_SOURCE),
         anyObject(), anyObject(), eq(Collections.emptyList()), eq(Collections.emptyList()),
@@ -75,7 +75,7 @@ public class WavefrontSpanTest {
   @Test
   public void testErrorWavefrontSpan() throws IOException, InterruptedException {
     String operationName = "dummyOp";
-    Map<String, String> pointTags = pointTags(operationName);
+    Map<String, String> pointTags = pointTags(operationName, new HashMap<>());
     WavefrontSender wfSender = createMock(WavefrontSender.class);
     wfSender.sendSpan(eq(operationName), anyLong(), anyLong(), eq(DEFAULT_SOURCE),
         anyObject(), anyObject(), eq(Collections.emptyList()), eq(Collections.emptyList()),
@@ -118,7 +118,95 @@ public class WavefrontSpanTest {
     verify(wfSender);
   }
 
-  private Map<String, String> pointTags(String operationName) {
+  @Test
+  public void testCustomRedMetricsTagsWavefrontSpan() throws IOException, InterruptedException {
+    String operationName = "dummyOp";
+    Map<String, String> pointTags = pointTags(operationName, new HashMap<String, String>() {{
+      put("tenant", "tenant1");
+      put("env", "Staging");
+    }});
+    WavefrontSender wfSender = createMock(WavefrontSender.class);
+    wfSender.sendSpan(eq(operationName), anyLong(), anyLong(), eq(DEFAULT_SOURCE),
+        anyObject(), anyObject(), eq(Collections.emptyList()), eq(Collections.emptyList()),
+        anyObject(), eq(null));
+    expectLastCall();
+
+    wfSender.sendMetric(eq(
+        "tracing.derived.myApplication.myService.dummyOp.invocation.count"),
+        eq(1.0), anyLong(), eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().atLeastOnce();
+
+    // TODO - change WavefrontInternalReporter.newWavefrontHistogram to pass in a clock to
+    // advance minute bin and change the below call to expectLastCall().atLeastOnce();
+    wfSender.sendDistribution(eq(
+        "tracing.derived.myApplication.myService.dummyOp.duration.micros"),
+        anyObject(), eq(new HashSet<>(Arrays.asList(HistogramGranularity.MINUTE))), anyLong(),
+        eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().anyTimes();
+
+    wfSender.sendMetric(eq(
+        "tracing.derived.myApplication.myService.dummyOp.total_time.millis.count"),
+        anyLong(), anyLong(), eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().atLeastOnce();
+
+    replay(wfSender);
+    WavefrontTracer tracer = new WavefrontTracer.Builder(
+        new WavefrontSpanReporter.Builder().withSource(DEFAULT_SOURCE).build(wfSender),
+        buildApplicationTags()).setReportFrequenceMillis(50).
+        redMetricsCustomTagKeys(new HashSet<>(Arrays.asList("tenant", "env"))).
+        build();
+    tracer.buildSpan("dummyOp").withTag("tenant", "tenant1").
+        withTag("env", "Staging").startActive(true).close();
+    // Sleep for 1 seconds
+    System.out.println("Sleeping for 1 second zzzzz .....");
+    Thread.sleep(1000);
+    System.out.println("Resuming execution .....");
+    verify(wfSender);
+  }
+
+  @Test
+  public void testNoCustomRedMetricsTagsWavefrontSpan() throws IOException, InterruptedException {
+    String operationName = "dummyOp";
+    Map<String, String> pointTags = pointTags(operationName, new HashMap<>());
+    WavefrontSender wfSender = createMock(WavefrontSender.class);
+    wfSender.sendSpan(eq(operationName), anyLong(), anyLong(), eq(DEFAULT_SOURCE),
+        anyObject(), anyObject(), eq(Collections.emptyList()), eq(Collections.emptyList()),
+        anyObject(), eq(null));
+    expectLastCall();
+
+    wfSender.sendMetric(eq(
+        "tracing.derived.myApplication.myService.dummyOp.invocation.count"),
+        eq(1.0), anyLong(), eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().atLeastOnce();
+
+    // TODO - change WavefrontInternalReporter.newWavefrontHistogram to pass in a clock to
+    // advance minute bin and change the below call to expectLastCall().atLeastOnce();
+    wfSender.sendDistribution(eq(
+        "tracing.derived.myApplication.myService.dummyOp.duration.micros"),
+        anyObject(), eq(new HashSet<>(Arrays.asList(HistogramGranularity.MINUTE))), anyLong(),
+        eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().anyTimes();
+
+    wfSender.sendMetric(eq(
+        "tracing.derived.myApplication.myService.dummyOp.total_time.millis.count"),
+        anyLong(), anyLong(), eq(DEFAULT_SOURCE), eq(pointTags));
+    expectLastCall().atLeastOnce();
+
+    replay(wfSender);
+    WavefrontTracer tracer = new WavefrontTracer.Builder(
+        new WavefrontSpanReporter.Builder().withSource(DEFAULT_SOURCE).build(wfSender),
+        buildApplicationTags()).setReportFrequenceMillis(50).
+        build();
+    tracer.buildSpan("dummyOp").withTag("tenant", "tenant1").
+        withTag("env", "Staging").startActive(true).close();
+    // Sleep for 1 seconds
+    System.out.println("Sleeping for 1 second zzzzz .....");
+    Thread.sleep(1000);
+    System.out.println("Resuming execution .....");
+    verify(wfSender);
+  }
+
+  private Map<String, String> pointTags(String operationName, Map<String, String> customTags) {
     return new HashMap<String, String>() {{
       put("application", "myApplication");
       put("service", "myService");
@@ -126,6 +214,7 @@ public class WavefrontSpanTest {
       put("shard", "none");
       put("component", "none");
       put("operationName", operationName);
+      putAll(customTags);
     }};
   }
 }
