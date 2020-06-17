24b4bbc08e6c1aac6de572ed5eca4f7bf827d3ac
akodali18 <akodali@vmware.com>
2020-06-17 11:09:33 -0700

    Force sample spans if debug=true is set.

--- src/test/java/com/wavefront/opentracing/WavefrontSpanTest.java
+++ src/test/java/com/wavefront/opentracing/WavefrontSpanTest.java
@@ -3,6 +3,7 @@ package com.wavefront.opentracing;
 import com.wavefront.opentracing.reporting.WavefrontSpanReporter;
 import com.wavefront.sdk.common.WavefrontSender;
 import com.wavefront.sdk.entities.histograms.HistogramGranularity;
+import com.wavefront.sdk.entities.tracing.sampling.RateSampler;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,7 @@ import io.opentracing.tag.Tags;
 
 import static com.wavefront.opentracing.Utils.buildApplicationTags;
 import static com.wavefront.opentracing.common.Constants.DEFAULT_SOURCE;
+import static com.wavefront.sdk.common.Constants.DEBUG_TAG_KEY;
 import static org.easymock.EasyMock.anyDouble;
 import static org.easymock.EasyMock.anyLong;
 import static org.easymock.EasyMock.anyObject;
@@ -81,6 +83,53 @@ public class WavefrontSpanTest {
   }
 
   @Test
+  public void testDebugWavefrontSpan() throws IOException, InterruptedException {
+    String operationName = "dummyOp";
+    Map<String, String> pointTags = pointTags(operationName, new HashMap<String, String>() {{
+      put("span.kind", "none");
+      put("debug","true");
+    }});
+    WavefrontSender wfSender = createMock(WavefrontSender.class);
+    wfSender.sendSpan(eq(operationName), anyLong(), anyLong(), eq(DEFAULT_SOURCE),
+        anyObject(), anyObject(), eq(Collections.emptyList()), eq(Collections.emptyList()),
+        anyObject(), eq(Collections.emptyList()));
+    expectLastCall();
+
+    wfSender.sendMetric(eq(
+        "~component.heartbeat"),
+        anyDouble(), anyLong(), eq(DEFAULT_SOURCE), anyObject());
+    expectLastCall().atLeastOnce();
+
+    wfSender.sendDeltaCounter(eq(
+        "∆tracing.derived.myApplication.myService.dummyOp.invocation.count"),
+        anyDouble(), eq(DEFAULT_SOURCE), anyObject());
+    expectLastCall().atLeastOnce();
+    // TODO - change WavefrontInternalReporter.newWavefrontHistogram to pass in a clock to
+    // advance minute bin and change the below call to expectLastCall().atLeastOnce();
+    wfSender.sendDistribution(eq(
+        "tracing.derived.myApplication.myService.dummyOp.duration.micros"),
+        anyObject(), eq(new HashSet<>(Arrays.asList(HistogramGranularity.MINUTE))), anyLong(),
+        eq(DEFAULT_SOURCE), anyObject());
+    expectLastCall().anyTimes();
+    wfSender.sendDeltaCounter(eq(
+        "∆tracing.derived.myApplication.myService.dummyOp.total_time.millis.count"),
+        anyLong(), eq(DEFAULT_SOURCE), anyObject());
+    expectLastCall().atLeastOnce();
+
+    replay(wfSender);
+    WavefrontTracer tracer = new WavefrontTracer.Builder(
+        new WavefrontSpanReporter.Builder().withSource(DEFAULT_SOURCE).build(wfSender),
+        buildApplicationTags()).setReportFrequenceMillis(50).
+        withSampler(new RateSampler(0.0)).build();
+    tracer.buildSpan("dummyOp").withTag(DEBUG_TAG_KEY, true).start().finish();
+    // Sleep for 60+ seconds
+    System.out.println("Sleeping for 1 second zzzzz .....");
+    Thread.sleep(1000);
+    System.out.println("Resuming execution .....");
+    verify(wfSender);
+  }
+
+  @Test
   public void testErrorWavefrontSpan() throws IOException, InterruptedException {
     String operationName = "dummyOp";
     Map<String, String> pointTags = pointTags(operationName, new HashMap<String, String>() {{
@@ -128,7 +177,8 @@ public class WavefrontSpanTest {
     replay(wfSender);
     WavefrontTracer tracer = new WavefrontTracer.Builder(
         new WavefrontSpanReporter.Builder().withSource(DEFAULT_SOURCE).build(wfSender),
-        buildApplicationTags()).setReportFrequenceMillis(50).build();
+        buildApplicationTags()).setReportFrequenceMillis(50).
+        withSampler(new RateSampler(0.0)).build();
     tracer.buildSpan("dummyOp").withTag(Tags.ERROR.getKey(), true).
         withTag("http.status_code", "404").start().finish();
     // Sleep for 60+ seconds
